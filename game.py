5b7326e549bc1c4baaddcc0bef223248808c7e89
Polito9 <colin.reyes.diego@gmail.com>
2023-08-14 12:48:44 -0600

    Turno del jugador sin validación y método para validar posición

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ psw_player = [] #Saves the positions where the ship is for the player map
 psw_computer = [] #Saves the positions where the ship is for the computer map
 player_map = []
 computer_map = []
-
+gameFinished = False
 def generateEmptyMap(): #We create an empty map
     my_map = []
     for i in range(10):#To create each row
@@ -70,10 +70,32 @@ def printCensoredMap(my_map): #We print any map that we pass it in the correct f
         print("") #Line jump
 
 
-def play():
+def validatePosition(x, y, map, psw): #Add the case if it is computer or player
+    mini_list = [x, y]
+    if (mini_list in psw): #Verifico que la posición indicada sea una de un barco
+        print("Acertaste!")
+        map[int(x)][int(y)] = " X "
+        psw.remove(mini_list)
+    else:
+        print("Lo siento, fallaste")
+        map[int(x)][int(y)] = " O " #cambio la variable
+
+
+def playPlayer_sTurn(): #Plays all the steps of the players turn
+    global psw_computer
     global computer_map
-    x = input("Ingresa tu coordenada x: ")
-    y = input("Ingresa su coordenada y")
+    x = int(input("Ingresa tu coordenada y: "))
+    y = int(input("Ingresa su coordenada x: "))
+    validatePosition(x, y, computer_map, psw_computer)
+
+def playComputer_sTurn(): #Plays and checks everything for making a good computer turn
+    #if there is no hint
+    global psw_computer
+    global player_map
+    x = random.randint(0, 9)
+    y = random.randint(0, 9)
+    validatePosition(x, y, player_map, psw_player)
+
 def startGame():
     global player_map
     global computer_map
@@ -89,4 +111,20 @@ def startGame():
     addShipstoMap(computer_map, 1, 3, psw_computer)
     addShipstoMap(computer_map, 2, 2, psw_computer)
 
+#Computers turn
+
 startGame()
+print(psw_computer)
+print("This is your game:")
+printAllMap(player_map)
+#Specify that cordinates start ar superior corner left. 
+while not gameFinished:
+    while len(psw_computer) > 0 or len(psw_player) > 0:
+        playPlayer_sTurn()
+        printCensoredMap(computer_map)
+        print("______________________________________")
+        #Turno de la computadora
+
+    else:
+        print("JUEGO TERMINADO, ESTOS SON LOS RESULTADOS: ")
+
